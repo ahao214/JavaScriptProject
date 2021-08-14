f3703c112461f1883b80acd723da98ea3c351fad
ahao214 <haochen214@163.com>
2021-08-14 20:09:27 +0800

    插入排序方法二

--- Algorithm/chapter03/InsertSort1.js
+++ Algorithm/chapter03/InsertSort1.js
@@ -32,3 +32,26 @@ for (k in arr) {
     txt += arr[k] + " ";
 }
 console.log("排序后：", txt);
+
+
+function insertSort2(arr) {
+    var key, left, right, middle;
+    for (var i = 1; i < arr.length; i++) {
+        key = arr[i];
+        left = 0;
+        right = i - 1;
+        while (left <= right) {
+            middle = Math.floor((left + right) / 2);
+            if (key < arr[middle]) {
+                right = middle - 1;
+            } else {
+                left = middle + 1;
+            }
+        }
+        for (var j = i - 1; j >= left; j--) {
+            arr[j + 1] = arr[j];
+        }
+        arr[left] = key;
+    }
+    return arr;
+}
