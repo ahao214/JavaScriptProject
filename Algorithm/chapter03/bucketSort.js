4b03168136f73afdbe35fb9695d78a80529eee1e
ahao214 <haochen214@163.com>
2021-08-30 17:04:39 +0800

    桶排序

--- Algorithm/chapter03/bucketSort.js
+++ Algorithm/chapter03/bucketSort.js
@@ -1,5 +1,36 @@
 /**桶排序 */
-function bucketSort(arr) {
+function bucketSort(arr, num) {
+    var len = arr.length;
+    if (len <= 1) {
+        return arr;
+    }
+    var buckets = [0],
+        result = [],
+        min = Math.min.apply(null, arr),
+        max = Math.max.apply(null, arr),
+        space = Math.ceil((max - min + 1) / num), //桶的数量
+        k, index;
+    for (var j = 0; j < len; j++) {
+        index = Math.floor((arr[j] - min) / space); //需要放入的桶的下标
+        if (buckets[index]) { //非空桶，执行插入排序
+            k = buckets[index].length - 1;
+            while (k >= 0 && buckets[index][k] > arr[j]) {
+                buckets[index][k + 1] = buckets[index][k];
+                k--;
+            }
+            buckets[index][k + 1] = arr[j];
+        } else { //空桶，执行初始化
+            buckets[index] = [];
+            buckets[index].push(arr[j]);
+        }
+    }
+    var n = 0;
+    //将所有的桶合并起来
+    while (n < num) {
+        result = result.concat(buckets[n]);
+        n++;
+    }
+    return result;
 
 }
 
@@ -12,7 +43,7 @@ for (var k in arr) {
 }
 
 console.log("排序前：", txt);
-arr = bucketSort(arr);
+arr = bucketSort(arr, 2);
 txt = "";
 
 for (k in arr) {
