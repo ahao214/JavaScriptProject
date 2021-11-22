d06d5fd9ba2e5fede252b97d9eb6837841dbc0ae
ahao214 <haochen214@163.com>
2021-11-22 13:25:15 +0800

    384. 打乱数组-暴力方法

---
+++ LeetCode/Medium/LC384/shuffle.js
@@ -0,0 +1,27 @@
+/**
+ * 384. 打乱数组
+ */
+
+//暴力方法
+var Solution = function(nums) {
+    this.nums = nums;
+    this.original = nums.slice();
+
+};
+
+Solution.prototype.reset = function() {
+    this.nums = this.original.slice();
+    return this.nums;
+};
+
+Solution.prototype.shuffle = function() {
+    const shuffled = new Array(this.nums.length).fill(0);
+    const list = [];
+    this.nums.forEach((num) => list.push(num));
+    for (let i = 0; i < this.nums.length; ++i) {
+        const j = Math.random() * list.length;
+        shuffled[i] = list.splice(j, 1);
+    }
+    this.nums = shuffled.slice();
+    return this.nums;
+};
