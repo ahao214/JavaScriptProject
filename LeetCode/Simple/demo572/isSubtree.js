d012758b779b7b5211b5f7fcb5b1b90e4551e5dd
ahao214 <haochen214@163.com>
2021-11-25 16:06:22 +0800

    572. 另一棵树的子树

---
+++ LeetCode/Simple/demo572/isSubtree.js
@@ -0,0 +1,18 @@
+/**
+ * 572. 另一棵树的子树
+ */
+
+function TreeNode(val, left, right) {
+    this.val = (val === undefined ? 0 : val)
+    this.left = (left === undefined ? null : left)
+    this.right = (right === undefined ? null : right)
+}
+
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} subRoot
+ * @return {boolean}
+ */
+var isSubtree = function(root, subRoot) {
+
+};
