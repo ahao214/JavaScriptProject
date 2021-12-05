265353546cfc84db685d4db6ee81147ce73c4327
ahao214 <haochen214@163.com>
2021-12-05 10:52:16 +0800

    372. 超级次方

--- LeetCode/Medium/demo372/superPow.js
+++ LeetCode/Medium/demo372/superPow.js
@@ -5,5 +5,16 @@
  * @return {number}
  */
 var superPow = function(a, b) {
-
+    const MOD = 1337;
+    let res = 1;
+    a %= MOD;
+    for (let i = b.length - 1; i >= 0; i--) {
+        let k = 1;
+        for (let j = 0; j < 10; j++) {
+            if (j == b[i]) res = res * k % MOD;
+            k = k * a % MOD;
+        }
+        a = k;
+    }
+    return res;
 };
