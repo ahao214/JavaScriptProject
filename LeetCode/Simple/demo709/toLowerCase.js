70a31e4afa140a53ca0ca490f2b805783ff1aef6
ahao214 <haochen214@163.com>
2021-12-12 00:19:53 +0800

    709. 转换成小写字母

--- LeetCode/Simple/demo709/toLowerCase.js
+++ LeetCode/Simple/demo709/toLowerCase.js
@@ -6,3 +6,14 @@
 var toLowerCase = function(s) {
     return s.toLowerCase();
 };
+
+var toLowerCase = function(s) {
+    const sb = [];
+    for (let ch of s) {
+        if (ch.charCodeAt() >= 65 && ch.charCodeAt() <= 90) {
+            ch = String.fromCharCode(ch.charCodeAt() | 32);
+        }
+        sb.push(ch);
+    }
+    return sb.join('');
+};
