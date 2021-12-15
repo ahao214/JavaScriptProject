57a37ecf722e628567767c62a7e368fec0ac630e
ahao214 <haochen214@163.com>
2021-12-15 14:25:12 +0800

    851. 喧闹和富有

--- LeetCode/Medium/demo851/loudAndRich.js
+++ LeetCode/Medium/demo851/loudAndRich.js
@@ -34,3 +34,40 @@ const dfs = (x, quiet, g, ans) => {
         }
     }
 };
+
+//拓扑排序
+var loudAndRich1 = function(richer, quiet) {
+    const n = quiet.length;
+    const g = new Array(n).fill(0);
+    for (let i = 0; i < n; ++i) {
+        g[i] = [];
+    }
+    const inDeg = new Array(n).fill(0);
+    for (const r of richer) {
+        g[r[0]].push(r[1]);
+        ++inDeg[r[1]];
+    }
+
+    const ans = new Array(n).fill(0);
+    for (let i = 0; i < n; ++i) {
+        ans[i] = i;
+    }
+    const q = [];
+    for (let i = 0; i < n; ++i) {
+        if (inDeg[i] === 0) {
+            q.push(i);
+        }
+    }
+    while (q.length) {
+        const x = q.shift();
+        for (const y of g[x]) {
+            if (quiet[ans[x]] < quiet[ans[y]]) {
+                ans[y] = ans[x]; // 更新 x 的邻居的答案
+            }
+            if (--inDeg[y] === 0) {
+                q.push(y);
+            }
+        }
+    }
+    return ans;
+};
