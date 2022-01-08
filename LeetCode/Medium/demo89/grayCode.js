afef05e076c6a543c2aace81035f42cdf4ea0bda
ahao214 <haochen214@163.com>
2022-01-08 10:07:37 +0800

    89. 格雷编码

--- LeetCode/Medium/demo89/grayCode.js
+++ LeetCode/Medium/demo89/grayCode.js
@@ -13,3 +13,11 @@ var grayCode = function(n) {
     }
     return res;
 };
+
+var grayCode1 = function(n) {
+    const ret = [];
+    for (let i = 0; i < 1 << n; i++) {
+        ret.push((i >> 1) ^ i);
+    }
+    return ret;
+};
