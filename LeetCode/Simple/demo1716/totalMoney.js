7ed001d133d58672e727718ad8fdd9489da08edb
ahao214 <haochen214@163.com>
2022-01-15 09:16:49 +0800

    1716. 计算力扣银行的钱

--- LeetCode/Simple/demo1716/totalMoney.js
+++ LeetCode/Simple/demo1716/totalMoney.js
@@ -25,3 +25,18 @@ var totalMoney = function(n) {
     }
     return res;
 };
+
+
+var totalMoney2 = function(n) {
+    // 所有完整的周存的钱
+    const weekNumber = Math.floor(n / 7);
+    const firstWeekMoney = Math.floor((1 + 7) * 7 / 2);
+    const lastWeekMoney = firstWeekMoney + 7 * (weekNumber - 1);
+    const weekMoney = Math.floor((firstWeekMoney + lastWeekMoney) * weekNumber / 2);
+    // 剩下的不能构成一个完整的周的天数里存的钱
+    const dayNumber = n % 7;
+    const firstDayMoney = 1 + weekNumber;
+    const lastDayMoney = firstDayMoney + dayNumber - 1;
+    const dayMoney = Math.floor((firstDayMoney + lastDayMoney) * dayNumber / 2);
+    return weekMoney + dayMoney;
+};
