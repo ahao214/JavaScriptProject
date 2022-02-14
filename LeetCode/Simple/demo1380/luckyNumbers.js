3cfb376915711bfb14276699f5aa2ebb2f8dbbc9
ahao214 <haochen214@163.com>
2022-02-15 01:07:31 +0800

    1380. 矩阵中的幸运数

--- LeetCode/Simple/demo1380/luckyNumbers.js
+++ LeetCode/Simple/demo1380/luckyNumbers.js
@@ -37,3 +37,26 @@ var luckyNumbers = function(matrix) {
     }
     return ret;
 };
+
+//方法二：预处理
+var luckyNumbers1 = function(matrix) {
+    const m = matrix.length,
+        n = matrix[0].length;
+    const minRow = new Array(m).fill(Number.MAX_SAFE_INTEGER);
+    const maxCol = new Array(n).fill(0);
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            minRow[i] = Math.min(minRow[i], matrix[i][j]);
+            maxCol[j] = Math.max(maxCol[j], matrix[i][j]);
+        }
+    }
+    const ret = [];
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (matrix[i][j] === minRow[i] && matrix[i][j] === maxCol[j]) {
+                ret.push(matrix[i][j]);
+            }
+        }
+    }
+    return ret;
+};
