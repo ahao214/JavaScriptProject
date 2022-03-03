f3fedb354e26668e621543565d30ca285d7f5eef
ahao214 <haochen214@163.com>
2022-03-04 01:59:44 +0800

    2104. 子数组范围和

--- LeetCode/Medium/demo2104/SubArrayRanges.js
+++ LeetCode/Medium/demo2104/SubArrayRanges.js
@@ -6,6 +6,7 @@
 
 子数组是数组中一个连续 非空 的元素序列。
 */
+//方法一：遍历子数组
 var subArrayRanges = function(nums) {
     const n = nums.length;
     let ret = 0;
@@ -19,4 +20,55 @@ var subArrayRanges = function(nums) {
         }
     }
     return ret;
+};
+
+//方法二：单调栈
+var subArrayRanges = function(nums) {
+    const n = nums.length;
+    const minLeft = new Array(n).fill(0);
+    const minRight = new Array(n).fill(0);
+    const maxLeft = new Array(n).fill(0);
+    const maxRight = new Array(n).fill(0);
+    let minStack = [];
+    let maxStack = [];
+    for (let i = 0; i < n; i++) {
+        while (minStack.length && nums[minStack[minStack.length - 1]] > nums[i]) {
+            minStack.pop();
+        }
+        minLeft[i] = minStack.length === 0 ? -1 : minStack[minStack.length - 1];
+        minStack.push(i);
+
+        // 如果 nums[maxStack[maxStack.length - 1]] == nums[i], 那么根据定义，
+        // nums[maxStack[maxStack.length - 1]] 逻辑上小于 nums[i]，因为 maxStack[maxStack.length - 1] < i
+        while (maxStack.length && nums[maxStack[maxStack.length - 1]] <= nums[i]) {
+            maxStack.pop();
+        }
+        maxLeft[i] = maxStack.length === 0 ? -1 : maxStack[maxStack.length - 1];
+        maxStack.push(i);
+    }
+    minStack = [];
+    maxStack = [];
+    for (let i = n - 1; i >= 0; i--) {
+        // 如果 nums[minStack[minStack.length - 1]] == nums[i], 那么根据定义，
+        // nums[minStack[minStack.length - 1]] 逻辑上大于 nums[i]，因为 minStack[minStack.length - 1] > i
+        while (minStack.length && nums[minStack[minStack.length - 1]] >= nums[i]) {
+            minStack.pop();
+        }
+        minRight[i] = minStack.length === 0 ? n : minStack[minStack.length - 1];
+        minStack.push(i);
+
+        while (maxStack.length && nums[maxStack[maxStack.length - 1]] < nums[i]) {
+            maxStack.pop();
+        }
+        maxRight[i] = maxStack.length === 0 ? n : maxStack[maxStack.length - 1];
+        maxStack.push(i);
+    }
+
+    let sumMax = 0,
+        sumMin = 0;
+    for (let i = 0; i < n; i++) {
+        sumMax += (maxRight[i] - i) * (i - maxLeft[i]) * nums[i];
+        sumMin += (minRight[i] - i) * (i - minLeft[i]) * nums[i];
+    }
+    return sumMax - sumMin;
 };
